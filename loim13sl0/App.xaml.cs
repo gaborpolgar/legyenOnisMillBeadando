dbaa5853f0edfbc736ed201a172136ec521cf571
Gábor Polgár <gabor.polgar@gmail.com>
2023-01-22 21:44:19 +0100

    fix

--- loim13sl0/App.xaml.cs
+++ loim13sl0/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace loim13sl0
+namespace legyenOnisMillBeadando
 {
     /// <summary>
     /// Interaction logic for App.xaml
